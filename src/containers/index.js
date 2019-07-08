a05ddcae265f50bbd7f1523d44e5254ecfd759d2
danny1105 <ashishtiwari11@outlook.com>
2019-07-08 11:27:01 +0530

    Added Basic Routes

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Index from "../components/Landing";
+import About from "../components/About";
+import Users from "../components/Users";
+import Events from "../components/Events";
 
 function Main() {
 	return (
-		<div className="App">
-			<h1>Ashish Hello</h1>
-		</div>
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+						<li>
+							<Link to="/users">Users</Link>
+						</li>
+						<li>
+							<Link to="/events">Events</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Route path="/" exact component={Index} />
+				<Route path="/about" component={About} />
+				<Route path="/users" component={Users} />
+				<Route path="/events" component={Events} />
+			</div>
+		</Router>
 	);
 }
 
