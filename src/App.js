4950dd7e863b37e9db7dbe6bab6f1916b210d5a1
ashish tiwari <ashishtiwari11@outlook.com>
2020-01-21 12:46:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React from "react";
 import Main from "./containers";
 
 function App() {
+	<script type="text/javascript">
+	function() {
+	    window.sib = {
+		equeue: [],
+		client_key: "p820drqfrd7pduakdibre29t"
+	    };
+	    /* OPTIONAL: email for identify request*/
+	    // window.sib.email_id = 'example@domain.com';
+	    window.sendinblue = {};
+	    for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
+	    (function(k) {
+		window.sendinblue[k] = function() {
+		    var arg = Array.prototype.slice.call(arguments);
+		    (window.sib[k] || function() {
+			    var t = {};
+			    t[k] = arg;
+			    window.sib.equeue.push(t);
+			})(arg[0], arg[1], arg[2]);
+		    };
+		})(j[i]);
+	    }
+	    var n = document.createElement("script"),
+		i = document.getElementsByTagName("script")[0];
+	    n.type = "text/javascript", n.id = "sendinblue-js", n.async = !0, n.src = "https://sibautomation.com/sa.js?key=" + window.sib.client_key, i.parentNode.insertBefore(n, i), window.sendinblue.page();
+	})();
 	return (
 		<div className="App">
 			<Main />
